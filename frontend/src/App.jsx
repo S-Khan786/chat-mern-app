f0394fdc5e7a133b0964ead9660eefb3b37f20f1
S-Khan786 <osahilkhan654@gmail.com>
2025-01-25 10:47:38 +0530

    Added vite@latest React+Javascript and install tailwindcss for Vite and daisyui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline text-green-400 ">
+      Hello world!
+    </h1>
+    <button className="btn btn-success text-amber-700">Success</button>
+    </>
+  )
+}
+
+export default App
