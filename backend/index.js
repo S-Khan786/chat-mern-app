843fea7905a748ff9f05025a647dc936eb848480
S-Khan786 <osahilkhan654@gmail.com>
2025-02-02 12:27:55 +0530

    changes made in index.js

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use('/api/user', userGet);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-appget("*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 })
 
